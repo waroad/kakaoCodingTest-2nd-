23da0daef05c9c1c3ac94d0d4b895949d5715d51
MrWon <won020725@naver.com>
2021-12-07 13:42:49 +0900

    Add to api.py a status code validation

--- insik/api.py
+++ insik/api.py
@@ -2,15 +2,33 @@ import requests
 import json
 
 class KakaoBikeAPI:
+    response_code_match: dict[tuple[bool, str]] = {
+        200: (True, "OK: success."),
+        400: (False, "Bad Request: Parameter has been wrong."),
+        401: (False, "Unauthorized: Authorization header has been wrong."),
+        500: (False, "Internal Server Error: Server gone wrong"),
+    }
+    
     def __init__(self, BASE_URL: str, X_AUTH_TOKEN: str, problem: int=1):
         self.base_url = BASE_URL
         self.x_auth_token = X_AUTH_TOKEN
         self.problem = problem
+        
+    def validate_or_raise(self, response: requests.Response) -> None:
+        match_result = KakaoBikeAPI.response_code_match.get(response.status_code)
+        
+        if (match_result is None):
+            raise Exception("status code is unexpected.")
+        
+        if (match_result[0] is False):
+            raise Exception(match_result[1])
 
     def start(self):
         res = requests.post(url=f'{self.base_url}/start',
                             headers={'X-Auth-Token': self.x_auth_token},
                             json={'problem': self.problem})
+        self.validate_or_raise(res)
+        
         start_params = res.json()
         self.auth_key = start_params['auth_key']
         self.time = start_params['time']
@@ -27,6 +45,7 @@ class KakaoBikeAPI:
             locations: tuple of (station id, bikes count)
         '''
         res = requests.get(url=f'{self.base_url}/locations', headers=self.auth_header)
+        self.validate_or_raise(res)
         
         return list(map(lambda loc: (loc['id'], loc['located_bikes_count']), 
                    res.json()['locations']))
@@ -39,6 +58,7 @@ class KakaoBikeAPI:
             trucks: tuple of (turck id, station id, loaded bikes count)
         '''
         res = requests.get(url=f'{self.base_url}/trucks', headers=self.auth_header)
+        self.validate_or_raise(res)
 
         return list(map(lambda truck: (truck['id'], truck['location_id'], truck['loaded_bikes_count']), 
                    res.json()['trucks']))
@@ -58,14 +78,16 @@ class KakaoBikeAPI:
         
         res = requests.put(url=f'{self.base_url}/simulate', 
                            headers=self.auth_header, 
-                           json={'commands': command_dict_list}).json()
+                           json={'commands': command_dict_list})
+        self.validate_or_raise(res)
         
-        self.status = res['status']
-        self.time = res['time']
-        self.failed_requests += int(float(res['failed_requests_count']))
-        self.distance = res['distance']    
+        res_json = res.json()
+        self.status = res_json['status']
+        self.time = res_json['time']
+        self.failed_requests += int(float(res_json['failed_requests_count']))
+        self.distance = res_json['distance']    
         
-        return int(float(res['failed_requests_count']))
+        return int(float(res_json['failed_requests_count']))
         
     def score(self) -> float:
         '''
@@ -75,6 +97,7 @@ class KakaoBikeAPI:
             score
         '''
         res = requests.get(url=f'{self.base_url}/score', headers=self.auth_header)
+        self.validate_or_raise(res)
         
         return float(res.json()['score'])
     
